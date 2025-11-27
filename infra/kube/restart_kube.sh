6ca216238e1633621f914f72f1e116fffbe202ee
Dmitry <komov93@gmail.com>
2025-11-27 04:28:32 +0300

    elastic+kibana+fluent bit prometheus+grafana

--- infra/kube/restart_kube.sh
+++ infra/kube/restart_kube.sh
@@ -28,3 +28,30 @@ kubectl port-forward svc/backend 4111:4111
 kubectl port-forward svc/selenoid 4444:4444
 kubectl port-forward svc/selenoid-ui 8080:8080
 
+# ШАГ 2: поднятие сервисов мониторинга
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts || true
+helm repo add elastic https://helm.elastic.co || true
+helm repo update
+
+helm upgrade --install monitoring prometheus-community/kube-prometheus-stack -n monitoring --create-namespace -f monitoring-values.yaml
+# ШАГ 3: поднятие сервисов логирования
+# Устанавливаем Elasticsearch
+helm install elasticsearch elastic/elasticsearch -f elastic-values.yaml --namespace logging --create-namespace --set singleNode=true
+# Установка Fluent Bit
+helm repo add fluent https://fluent.github.io/helm-charts
+helm repo update
+
+helm install fluent-bit fluent/fluent-bit -f fluent-bit-values.yaml --namespace logging --create-namespace
+# Устанавливаем Kibana
+helm install kibana elastic/kibana -f kibana-values.yaml --namespace logging --create-namespace
+# Пробрасываем порт к прометеусу и графане и кибане
+kubectl port-forward svc/monitoring-kube-prometheus-prometheus -n monitoring 3001:9090 # > /dev/null 2>&1
+kubectl port-forward svc/monitoring-grafana -n monitoring 3002:80
+kubectl port-forward svc/kibana-kibana -n logging 5601:5601
+# Создаем секреты для авторизации на бекенде
+kubectl create secret generic backend-basic-auth --from-literal=username=admin --from-literal=password=admin -n monitoring
+# Применяем yaml с настройкой SpringMonitoring за бекендом
+kubectl apply -f spring-monitoring.yaml
+
+
+
